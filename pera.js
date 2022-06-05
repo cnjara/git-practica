c47c0c5fb32d32eabf593cb4774b649c12f3389b
cnjara <cnjara@outlook.com>
2022-06-04 23:56:03 -0300

    Eliminar duplicados en peras

--- pera.js
+++ pera.js
@@ -1,6 +1,5 @@
 console.log( 'pera' );
 console.log('fruta');
 console.log('al vino tinto');
-console.log('en almibar');
-console.log('compota de pera');
 console.log('pera con almibar');
+console.log('compota de pera');
